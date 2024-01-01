929aeb804828f0f4f0ade4f5640670a0c748ebec
helastro <helmyfirdy888@gmail.com>
2024-01-01 23:55:39 +0700

    feat - Section Artikel

--- src/js/index.js
+++ src/js/index.js
@@ -69,3 +69,32 @@ rightArrow.addEventListener("click", () => {
     updateActiveItem();
   }
 });
+
+const articleBtn = document.getElementById("articleBtn");
+const fotoBtn = document.getElementById("fotoBtn");
+const videoBtn = document.getElementById("videoBtn");
+
+articleBtn.addEventListener("click", () => toggleActive(articleBtn));
+fotoBtn.addEventListener("click", () => toggleActive(fotoBtn));
+videoBtn.addEventListener("click", () => toggleActive(videoBtn));
+
+function toggleActive(clickedBtn) {
+  articleBtn.classList.remove("active");
+  fotoBtn.classList.remove("active");
+  videoBtn.classList.remove("active");
+
+  clickedBtn.classList.add("active");
+  clickedBtn.style.borderRadius = "100px";
+  clickedBtn.style.border = "1px solid #FFF";
+  clickedBtn.style.background = "rgba(255, 255, 255, 0.50)";
+
+  [articleBtn, fotoBtn, videoBtn].forEach((btn) => {
+    if (btn !== clickedBtn) {
+      btn.style.borderRadius = "";
+      btn.style.border = "";
+      btn.style.background = "";
+    }
+  });
+}
+
+toggleActive(articleBtn);
